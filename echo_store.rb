e56352306b40d1a38858259e96a32a1076e62879
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2017-11-22 07:32:08 +0500

    Echo store: Added hours logic

--- echo_store.rb
+++ echo_store.rb
@@ -7,6 +7,7 @@ class EchoStore
 
   STORE_URL = 'https://www.ecostore.it/app/themes/pn-theme/models/services/get-all-locations.php'
   STORE_DETAIL = 'https://www.ecostore.it/store?id='
+  WEEKDAYS = { "LUN" => 0, "MAR" => 1, "MER" => 2, "GIO" => 3, "VEN" => 4, "SAB" => 5, "DOM" => 6 }
 
   def get_stores
     all_stores = []
@@ -24,7 +25,6 @@ class EchoStore
       detail_url = STORE_DETAIL + store_data["store_code"].to_s
       store_detail = Nokogiri::HTML(open(detail_url))
       store[:phone_number] = store_detail.css('.single-store__rec a').text
-      byebug
       store[:hours] = get_hours store_detail
       all_stores << store
     end
@@ -32,9 +32,31 @@ class EchoStore
   end
 
   def get_hours(store)
-    byebug
-   hours_table = store.css('.single-store__table-list')
+    hours_data = []
+    hours_table = store.css('.single-store__table-list li')
+    hours_table.each do |day|
+      day_name = day.children[0].text
+      p "hour_data: "+ day.text
+      daily_hours = {}
+      daily_hours[:weekday] = WEEKDAYS[day_name]
+      hours_data << parse_hours(day, daily_hours)
+    end
+    hours_data
   end
+
+  def parse_hours(day, daily_hours)
+    morning_hours = day.children[3].text.split(" ")
+    return daily_hours if morning_hours.size == 1 && morning_hours[0] == 'closed'
+    daily_hours[:open_morning] = morning_hours[0]
+    daily_hours[:close_morning] = morning_hours[1]
+    if day.children[6]
+      evening_hours = day.children[6].text.split(" ")
+      daily_hours[:open_evening] = evening_hours[0]
+      daily_hours[:close_evening] = evening_hours[1]
+    end
+    daily_hours
+  end
+
 
 
   def run
@@ -42,8 +64,6 @@ class EchoStore
     # PQSDK::Settings.host = 'api.promoqui.eu'
     # PQSDK::Settings.app_secret = '1904fbed9987ee6cd5653d558f8ad9e8ce281f94bc01a44b50adc64fbc95d612'
     stores = get_stores
-    p "*"*100
-    stores.each do |s| p s[:city] end
   end
 end
 
